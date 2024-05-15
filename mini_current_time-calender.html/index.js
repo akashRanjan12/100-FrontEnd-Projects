6abe8aff80d059de372f16a052dd91e12d094950
akashRaanjan12 <akash12ranjan@gmail.com>
2024-05-16 01:04:37 +0530

    add:current-day Calender

---
+++ mini_current_time-calender.html/index.js
@@ -0,0 +1,15 @@
+var monthEl = document.querySelector(".month");
+var weekEl = document.querySelector(".week");
+var dateEl = document.querySelector(".date");
+var yearEl = document.querySelector(".year");
+
+var date = new Date();
+var month = date.getMonth();
+monthEl.innerHTML = date.toLocaleString("eng", {
+  month: "long",
+});
+weekEl.innerHTML = date.toLocaleString("eng", {
+  weekday: "long",
+});
+dateEl.innerHTML = date.getDay();
+yearEl.innerHTML = date.getFullYear();
